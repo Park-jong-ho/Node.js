69e397c23d2ef033f77a5e78ed50d6b7cf372468
jongho <whjk1104@gmail.com>
2024-12-16 15:16:38 +0900

    ✨ Session Final
    Session Final

--- 12.16.Session/server.js
+++ 12.16.Session/server.js
@@ -8,15 +8,16 @@ const users = [
     user_id: 'test',
     user_password: '1234',
     user_name: '테스트 유저',
-    user_info: '테스터 유저입니다'
+    user_info: '테스트 유저입니다'
   }
 ]
 
 const app = express()
+
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
-  methods: ['OPTIONS', 'POST', 'GET', 'DELETE'],
-  credential: true
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  methods: ['OPTIONS', 'POST', "GET", "DELETE"],
+  credentials: true
 }))
 
 app.use(cookieParser())
@@ -25,11 +26,31 @@ app.use(express.json())
 app.use(session({
   secret: 'session secret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  name: 'session_id'
 }))
 
 app.post('/', (req, res) => {
-  console.log(req.body);
+  const { userId, userPassword } = req.body
+  const userInfo = users.find(el => el.user_id === userId && el.user_password === userPassword)
+
+  if (!userInfo) {
+    res.status(401).send('로그인 실패')
+  } else {
+    req.session.userId = userInfo.user_id
+    res.send("세션 생성 완료!")
+  }
 })
 
-app.listen(3000, () => console.log('서버 실행'))
+app.get('/', (req, res) => {
+  const userInfo = users.find(el => el.user_id === req.session.userId) 
+  return res.json(userInfo)
+})
+
+app.delete('/', (req, res) => {
+  req.session.destroy()
+  res.clearCookie('session_id')
+  res.send("세션 삭제 완료!")
+})
+
+app.listen(3000, () => console.log('서버 실행!'))
